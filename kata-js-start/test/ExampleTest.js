7b5ba13df114400ea29bbfeebfa088f9d65fb80d
Celebom <cedric.debord@gmail.com>
2022-05-05 18:07:57 +0200

    Solution javascript

---
+++ kata-js-start/test/ExampleTest.js
@@ -0,0 +1,7 @@
+import { fizzBuzz } from '../functions/functions.js';
+
+describe('FizzBuzz exercise', function () {
+    it('Execute FizzBuzz code', function () {
+        fizzBuzz();
+    });
+});
